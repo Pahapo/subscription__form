28c82c6aca3cb3fb582ad0fa5c2ed14ca522037b
Pavel <83354660+Pahapo@users.noreply.github.com>
2023-04-24 13:25:40 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -17,3 +17,35 @@ examination.addEventListener("click", () => {
   const name = "@" + document.querySelector(".text__input input").value;
   console.log("name: " + name);
 });
+
+/* const check = document.querySelector(".subscription__check");
+const form = document.querySelector(".subscription__form");
+const closeTelegram = document.querySelector(".telegram__close");
+
+const examination = document.querySelector(".examination");
+
+const nextPage = document.getElementById("next__page");
+
+let toggel = true;
+
+function openNextPage() {
+  nextPage.href = "info.html";
+  toggel = false;
+}
+
+check.addEventListener("click", () => {
+  if (toggel) {
+    form.style.display = "block";
+    setTimeout(openNextPage, 15000);
+
+    closeTelegram.addEventListener("click", () => {
+      form.style.display = "none";
+    });
+
+    examination.addEventListener("click", () => {
+      const name = "@" + document.querySelector(".text__input input").value;
+      console.log("name: " + name);
+    });
+  }
+});
+*/
