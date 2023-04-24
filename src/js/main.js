93176fa4c771d254f93062aa4c8ce12d2bb4ddff
Pavel <83354660+Pahapo@users.noreply.github.com>
2023-04-24 12:24:18 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@ const check = document.querySelector(".subscription__check");
 const form = document.querySelector(".subscription__form");
 const closeTelegram = document.querySelector(".telegram__close");
 
+const examination = document.querySelector(".examination");
+
 check.addEventListener("click", () => {
   form.style.display = "block";
 });
@@ -9,4 +11,9 @@ check.addEventListener("click", () => {
 closeTelegram.addEventListener("click", () => {
   form.style.display = "none";
   console.log("close form");
+});
+
+examination.addEventListener("click", () => {
+  const name = "@" + document.querySelector(".text__input input").value;
+  console.log("name: " + name);
 });
